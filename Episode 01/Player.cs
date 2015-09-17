556fcdce0f59e0f381fdd1252bfcd70b88d0d1ea
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2015-09-17 23:17:37 +0200

    E01

---
+++ Episode 01/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (PlayerController))]
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 5;
+
+	Camera viewCamera;
+	PlayerController controller;
+	
+	void Start () {
+		controller = GetComponent<PlayerController> ();
+		viewCamera = Camera.main;
+	}
+
+	void Update () {
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		controller.Move (moveVelocity);
+
+		Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayDistance;
+
+		if (groundPlane.Raycast(ray,out rayDistance)) {
+			Vector3 point = ray.GetPoint(rayDistance);
+			Debug.DrawLine(ray.origin,point,Color.red);
+			//Debug.DrawRay(ray.origin,ray.direction * 100,Color.red);
+			controller.LookAt(point);
+		}
+	}
+}
