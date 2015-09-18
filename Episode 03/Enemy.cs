e1083707029f43dbde8c235f159f3a40649ce02a
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2015-09-18 22:40:19 +0200

    Added episodes 3 through 5

---
+++ Episode 03/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class Enemy : MonoBehaviour {
+
+	NavMeshAgent pathfinder;
+	Transform target;
+
+	void Start () {
+		pathfinder = GetComponent<NavMeshAgent> ();
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		StartCoroutine (UpdatePath ());
+	}
+
+	void Update () {
+
+	}
+
+	IEnumerator UpdatePath() {
+		float refreshRate = .25f;
+
+		while (target != null) {
+			Vector3 targetPosition = new Vector3(target.position.x,0,target.position.z);
+			pathfinder.SetDestination (targetPosition);
+			yield return new WaitForSeconds(refreshRate);
+		}
+	}
+}
---
+++ Episode 05/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class Enemy : LivingEntity {
+
+	NavMeshAgent pathfinder;
+	Transform target;
+
+	protected override void Start () {
+		base.Start ();
+		pathfinder = GetComponent<NavMeshAgent> ();
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		StartCoroutine (UpdatePath ());
+	}
+
+	void Update () {
+
+	}
+
+	IEnumerator UpdatePath() {
+		float refreshRate = .25f;
+
+		while (target != null) {
+			Vector3 targetPosition = new Vector3(target.position.x,0,target.position.z);
+			if (!dead) {
+				pathfinder.SetDestination (targetPosition);
+			}
+			yield return new WaitForSeconds(refreshRate);
+		}
+	}
+}
