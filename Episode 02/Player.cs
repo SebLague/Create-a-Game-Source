48bb6bcc2fff6993122b834065bf53d29edc33a4
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2015-09-18 21:40:08 +0200

    Added E02

---
+++ Episode 02/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (PlayerController))]
+[RequireComponent (typeof (GunController))]
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 5;
+
+	Camera viewCamera;
+	PlayerController controller;
+	GunController gunController;
+	
+	void Start () {
+		controller = GetComponent<PlayerController> ();
+		gunController = GetComponent<GunController> ();
+		viewCamera = Camera.main;
+	}
+
+	void Update () {
+		// Movement input
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		controller.Move (moveVelocity);
+
+		// Look input
+		Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayDistance;
+
+		if (groundPlane.Raycast(ray,out rayDistance)) {
+			Vector3 point = ray.GetPoint(rayDistance);
+			//Debug.DrawLine(ray.origin,point,Color.red);
+			controller.LookAt(point);
+		}
+
+		// Weapon input
+		if (Input.GetMouseButton(0)) {
+			gunController.Shoot();
+		}
+	}
+}
---
+++ Episode 02/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	float speed = 10;
+
+	public void SetSpeed(float newSpeed) {
+		speed = newSpeed;
+	}
+	
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+	}
+}
