d2e3d022feff865d642cfd9e85bf127fa434d345
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2015-10-18 18:06:14 +0200

    Added e15 and e16

---
+++ Episode 16/Gun.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public Transform muzzle;
+	public Projectile projectile;
+	public float msBetweenShots = 100;
+	public float muzzleVelocity = 35;
+
+	public Transform shell;
+	public Transform shellEjection;
+	MuzzleFlash muzzleflash;
+	float nextShotTime;
+
+	void Start() {
+		muzzleflash = GetComponent<MuzzleFlash> ();
+	}
+	
+	public void Shoot() {
+
+		if (Time.time > nextShotTime) {
+			nextShotTime = Time.time + msBetweenShots / 1000;
+			Projectile newProjectile = Instantiate (projectile, muzzle.position, muzzle.rotation) as Projectile;
+			newProjectile.SetSpeed (muzzleVelocity);
+
+			Instantiate(shell,shellEjection.position, shellEjection.rotation);
+			muzzleflash.Activate();
+		}
+	}
+}
