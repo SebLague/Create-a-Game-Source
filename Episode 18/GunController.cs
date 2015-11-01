1e78251bb73a0c573c39f18cd258e09adf18ee2a
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2015-11-01 14:05:34 +0200

    ep 17 + 18

---
+++ Episode 18/GunController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+
+	public Transform weaponHold;
+	public Gun startingGun;
+	Gun equippedGun;
+
+	void Start() {
+		if (startingGun != null) {
+			EquipGun(startingGun);
+		}
+	}
+
+	public void EquipGun(Gun gunToEquip) {
+		if (equippedGun != null) {
+			Destroy(equippedGun.gameObject);
+		}
+		equippedGun = Instantiate (gunToEquip, weaponHold.position,weaponHold.rotation) as Gun;
+		equippedGun.transform.parent = weaponHold;
+	}
+
+	public void OnTriggerHold() {
+		if (equippedGun != null) {
+			equippedGun.OnTriggerHold();
+		}
+	}
+
+	public void OnTriggerRelease() {
+		if (equippedGun != null) {
+			equippedGun.OnTriggerRelease();
+		}
+	}
+}
---
+++ Episode 18/Shell.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shell : MonoBehaviour {
+
+	public Rigidbody myRigidbody;
+	public float forceMin;
+	public float forceMax;
+
+	float lifetime = 4;
+	float fadetime = 2;
+
+	void Start () {
+		float force = Random.Range (forceMin, forceMax);
+		myRigidbody.AddForce (transform.right * force);
+		myRigidbody.AddTorque (Random.insideUnitSphere * force);
+
+		StartCoroutine (Fade ());
+	}
+	
+	IEnumerator Fade() {
+		yield return new WaitForSeconds(lifetime);
+
+		float percent = 0;
+		float fadeSpeed = 1 / fadetime;
+		Material mat = GetComponent<Renderer> ().material;
+		Color initialColour = mat.color;
+
+		while (percent < 1) {
+			percent += Time.deltaTime * fadeSpeed;
+			mat.color = Color.Lerp(initialColour, Color.clear, percent);
+			yield return null;
+		}
+
+		Destroy (gameObject);
+	}
+}
